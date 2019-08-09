0933a28d4c244df553b66d782e0693703b3f1d03
github's Name <zl546505720@163.com>
2019-08-09 16:20:28 +0800

    spring

---
+++ Demo15/src/main/java/com/itheima/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.itheima.dao;
+
+public class UserDao {
+    public void userDao(){
+        System.out.println("我的第一个maven程序");
+    }
+}
---
+++ Demo15/src/test/java/com/itheima/dao/UserDaoTest.java
@@ -0,0 +1,12 @@
+package com.itheima.dao;
+
+import static org.junit.Assert.*;
+
+public class UserDaoTest {
+
+    @org.junit.Test
+    public void userDao() {
+        UserDao dao = new UserDao();
+        dao.userDao();
+    }
+}
